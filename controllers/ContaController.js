4a120e496742e5d8d79df23d8ba6d56fc7c8fd12
Renato Monteiro Batista <r3n4t0@gmail.com>
2020-06-18 12:33:24 -0300

    Criação do controller para as contas

---
+++ controllers/ContaController.js
@@ -0,0 +1,30 @@
+const conta = require('../models/conta')
+const { check, validationResult } = require('express-validator');
+
+module.exports = {
+   async create(req,res){        
+      
+      await check('nome').exists().run(req);
+      await check('banco_nome').exists().run(req);
+      await check('banco_code').exists().run(req);
+      await check('numero').exists().run(req);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(422).json({ processado: false, mensagem: "Erro na validacao dos dados, preencha todos os campos." });
+      }
+      conta.create({
+         nome:req.body.nome,
+         banco_nome:req.body.banco_nome,
+         banco_code:req.body.banco_code,
+         numero:req.body.numero,
+         userId:req.userId,
+         created_at: Date()
+      }).then(resposta =>  res.status(200).json({ processado: true, mensagem: "Conta cadastrada com sucesso."}))
+      .catch(err => res.status(400).json({'error':'ocorreu um erro no cadastramento da conta'}))
+   },
+   async listarTodas(req,res){
+      let contas = await conta.find({userId:req.userId})
+      res.json(contas)
+      
+  }
+}
